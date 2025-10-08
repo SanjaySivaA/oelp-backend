935aa02ccfdc9cb2d3923a8435e17f799e228e86
Karthik_Krishna_M_CS <168297968+KarthikKrishnaMCS@users.noreply.github.com>
2025-10-09 00:57:31 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     ALLOWED_ORIGINS: list[str] = []
-    CORS_ORIGIN_REGEX: str = ""
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    GOOGLE_API_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
