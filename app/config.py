7b49f86038d0c1322ee625bd6a3407ee03f447a6
Sanjay Siva <realsanjaysiva@gmail.com>
2025-11-24 22:21:07 +0530

    cors

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Settings(BaseSettings):
     DATABASE_URL: str
     ALLOWED_ORIGINS: list[str] = []
+    CORS_ORIGIN_REGEX: str = ""
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
